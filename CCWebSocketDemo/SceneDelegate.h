c9513b174265f72d39b364c5c0c0d6dda526f060
CC (deng you hua) <ccworld1000@gmail.com>
2019-11-15 14:12:41 +0800

    CCAutoTag push : ws 1

---
+++ CCWebSocketDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  CCWebSocketDemo
+//
+//  Created by CC on 2019/11/15.
+//  Copyright © 2019 CC (deng you hua | cworld1000@gmail.com). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ CCWebSocketDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCWebSocketDemo
+//
+//  Created by CC on 2019/11/15.
+//  Copyright © 2019 CC (deng you hua | cworld1000@gmail.com). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
